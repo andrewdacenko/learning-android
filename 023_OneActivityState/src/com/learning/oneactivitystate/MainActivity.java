9bdd76a6d70cc8380d8247f09b5ebf6da49b42dd
Andrew <a.s.dvornik@gmail.com>
2013-03-29 15:24:32 +0200

    Added main state methods to MainActivity

--- 023_OneActivityState/src/com/learning/oneactivitystate/MainActivity.java
+++ 023_OneActivityState/src/com/learning/oneactivitystate/MainActivity.java
@@ -1,15 +1,48 @@
 package com.learning.oneactivitystate;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+
+	final String TAG = "States";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(TAG, "MainActivity: onStart()");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "MainActivity: onResume()");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(TAG, "MainActivity: onPause()");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(TAG, "MainActivity: onStop()");
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "MainActivity: onDestroy()");
 	}
 
 	@Override
