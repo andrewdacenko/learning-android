c40526d5baf3d3060a0b000e9dcf6f867f4c7495
Andrew <a.s.dvornik@gmail.com>
2013-03-29 14:38:55 +0200

    Edited MainActivity in Two Screens

--- 021_TwoScreens/src/com/learnimg/twoscreens/MainActivity.java
+++ 021_TwoScreens/src/com/learnimg/twoscreens/MainActivity.java
@@ -1,23 +1,41 @@
 package com.learnimg.twoscreens;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+public class MainActivity extends Activity implements OnClickListener {
+	Button btnActTwo;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		btnActTwo = (Button) findViewById(R.id.btnActTwo);
+		btnActTwo.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btnActTwo:
+			// TODO Call second activity
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
