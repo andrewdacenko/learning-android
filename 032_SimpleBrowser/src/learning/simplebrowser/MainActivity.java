6b9d9ec12aeaa21ba8324626168fde1f828b6daf
Andrew <a.s.dvornik@gmail.com>
2013-04-13 20:04:57 +0300

    Changed layout and Main Activity

--- 032_SimpleBrowser/src/learning/simplebrowser/MainActivity.java
+++ 032_SimpleBrowser/src/learning/simplebrowser/MainActivity.java
@@ -1,8 +1,12 @@
 package learning.simplebrowser;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		(findViewById(R.id.btnWeb)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri
+						.parse("http://www.google.com"))); 
+			}
+		});
 	}
 
 	@Override
