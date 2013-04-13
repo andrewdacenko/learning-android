f0584533336118950b5b16b65abcf579fd19957b
Andrew <a.s.dvornik@gmail.com>
2013-04-13 18:40:20 +0300

    Changed MainActivity

--- 030_MultipleActivityResults/src/learning/multipleactivityresults/MainActivity.java
+++ 030_MultipleActivityResults/src/learning/multipleactivityresults/MainActivity.java
@@ -1,15 +1,39 @@
 package learning.multipleactivityresults;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	TextView tvText;
+	Button btnColor;
+	Button btnAlign;
+
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		tvText = (TextView) findViewById(R.id.tvText);
+
+		btnColor = (Button) findViewById(R.id.btnColor);
+		btnAlign = (Button) findViewById(R.id.btnAlign);
+
+		btnColor.setOnClickListener(this);
+		btnAlign.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
