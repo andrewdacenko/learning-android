b342c902bb257a9c97e5e76983315902983a9522
Andrew <a.s.dvornik@gmail.com>
2013-04-13 18:49:11 +0300

    Changed MainActivity to process results

--- 030_MultipleActivityResults/src/learning/multipleactivityresults/MainActivity.java
+++ 030_MultipleActivityResults/src/learning/multipleactivityresults/MainActivity.java
@@ -1,14 +1,22 @@
 package learning.multipleactivityresults;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
+
+	final int REQUEST_CODE_COLOR = 1;
+	final int REQUEST_CODE_ALIGN = 2;
 
 	TextView tvText;
 	Button btnColor;
@@ -32,8 +40,40 @@ public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
+		Intent intent;
+		switch (v.getId()) {
+		case R.id.btnColor:
+			intent = new Intent(this, ColorActivity.class);
+			startActivityForResult(intent, REQUEST_CODE_COLOR);
+			break;
+		case R.id.btnAlign:
+			intent = new Intent(this, AlignActivity.class);
+			startActivityForResult(intent, REQUEST_CODE_ALIGN);
+			break;
+		}
+	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// запишем в лог значения requestCode и resultCode
+		Log.d("myLogs", "requestCode = " + requestCode + ", resultCode = "
+				+ resultCode);
+		// если пришло ОК
+		if (resultCode == RESULT_OK) {
+			switch (requestCode) {
+			case REQUEST_CODE_COLOR:
+				int color = data.getIntExtra("color", Color.WHITE);
+				tvText.setTextColor(color);
+				break;
+			case REQUEST_CODE_ALIGN:
+				int align = data.getIntExtra("alignment", Gravity.LEFT);
+				tvText.setGravity(align);
+				break;
+			}
+			// если вернулось не ОК
+		} else {
+			Toast.makeText(this, "Wrong result", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	@Override
